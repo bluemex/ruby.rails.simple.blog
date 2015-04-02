6b113efeb87a76582c442aaa832b2a27f479b21c
Jin <bluemex.jin@gmail.com>
2015-04-02 23:04:40 +0800

    add omniauth-google-oauth2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,6 +233,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :google_oauth2, "246924513521-8v0201rpk6tmqgv1v173bc9uceeve6u2.apps.googleusercontent.com", "eSwC_tOa0SODMGtXpaFMl2I5", { }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_config = ActiveAdmin::Devise.config
+  devise_config[:controllers][:omniauth_callbacks] = "omniauth_callbacks"
+  devise_for :admin_users, devise_config
   ActiveAdmin.routes(self)
+
   devise_for :users
+
   resources :articles do
     resources :comments
   end
@@ -13,50 +17,4 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   get 'index' => 'articles#index'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
