400af65695164d6b6623a9e4943037da47f23a79
Jin <bluemex.jin@gmail.com>
2015-04-03 01:19:39 +0800

    update activeadmin & css

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -1,18 +1,31 @@
 ActiveAdmin.register Article do
 
+permit_params :title, :content
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :content
+    column :user_id
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  filter :title
+  filter :content
+  filter :user_id
+  filter :created_at
+  filter :updated_at
+
+  form do |f|
+    f.inputs "article Details" do
+      f.input :title
+      f.input :content
+    end
+    f.actions
+  end
 
 
 end
